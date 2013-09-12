49ad252968a138f60c4d0a43b924a85f4a5a3ae5
clux <analsandblaster@gmail.com>
2013-09-12 10:52:48 +0100

    throw if missing external config

--- irc.js
+++ irc.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 var cfgPath = require('confortable')('.wa.json', process.cwd());
 
-console.log('using: ' + cfgPath);
+if (!cfgPath) {
+  throw new Error("When loading wolfram-irc externally, a local config is required");
+}
+console.log('using config: ' + cfgPath);
 var cfg = require(cfgPath);
 
 var bot = require('gu')(cfg.server, cfg.name, {
